9e2f62b010bca270e693f3b9827b74cd99df6b48
Hardik Malhotra <thehkmalhotra@Hardiks-MacBook-Pro.local>
2025-05-15 01:42:35 +0530

    15th May

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const products = require("./products/index");
 const cities = require("./cities/index");
 const discounts = require("./discounts/index");
 const orders = require("./orders/index");
+const search = require("./search/index");
 const adminProducts = require("./admin/products/index");
 
 router.use("/collections", collections);
@@ -13,6 +14,7 @@ router.use("/products", products);
 router.use("/cities", cities);
 router.use("/discounts", discounts);
 router.use("/orders", orders);
+router.use("/search", search);
 router.use("/admin/products", adminProducts);
 
 module.exports = router;
