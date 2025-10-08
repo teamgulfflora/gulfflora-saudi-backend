dc2e54f63b2d777d53f52f5203a3a9101c6d678b
Hardik Malhotra <hardikmalhotra2000@gmail.com>
2025-10-09 04:39:43 +0530

    9th October

--- routes/mailer/index.js
+++ routes/mailer/index.js
@@ -30,6 +30,7 @@ router.post("/send", async (req, res) => {
     });
     const tokenData = await tokenRes.json();
     const accessToken = tokenData.access_token;
+    const htmlContent = Buffer.from(body, "utf8").toString();
     const mailRes = await fetch("https://api.sendpulse.com/smtp/emails", {
       method: "POST",
       headers: {
@@ -38,7 +39,7 @@ router.post("/send", async (req, res) => {
       },
       body: JSON.stringify({
         email: {
-          html: body,
+          html: htmlContent,
           subject,
           from: { name: "Gulfflora", email: process.env.SENDPULSE_SMTP_EMAIL },
           to: [{ email: recipient }],
